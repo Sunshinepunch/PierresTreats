f180a1d309696dd1dabe4f39ba209ef0a8bf83c9
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-10-22 14:35:09 -0700

    home index splash contains viewbag for both flavors and treats - taking break to attend to homelife, will continue to work to full later!

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -5,9 +5,17 @@ namespace Bakery.Controllers
     public class HomeController : Controller
     {
 
+    private readonly BakeryContext _db;
+
+    public HomeController(BakeryContext db)
+    {
+        _db = db;
+    }
       [HttpGet("/")]
       public ActionResult Index()
       {
+        ViewBag.Flavors = _db.Flavors.ToList();
+        ViewBag.Treats = _db.Treats.ToList();
         return View();
       }
 
